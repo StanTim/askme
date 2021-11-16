cb7868ff46a44969d0f69e1fccb20d42ac8b376f
Stanislav A. Timanov <1694569@mail.ru>
2021-11-16 20:52:46 +0300

    added pass and email validation for user

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,24 @@
 class Question < ApplicationRecord
+
+  belongs_to :user
+
+  validates :text, presence: true
+  validates :text, length: { maximum: 255 }
+
+  # Демонстрация жизненного цикла объекта
+  # колбэки
+  # before_validation :before_validation
+  # after_validation :after_validation
+  # before_save :before_save
+  # before_create :before_create
+  # after_create :after_create
+  # after_save :after_save
+  #
+  # before_update :before_update
+  # after_update :after_update
+  #
+  # before_destroy :before_destroy
+  # after_destroy :after_destroy
+  #
+  # private
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,62 @@
-class User < ApplicationRecord
+require 'openssl'
 
+class User < ApplicationRecord
+  # Параметры алгоритма шифрования
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # создаем соль - рандомная строка усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # Создаём хэш пароля - длинная уникальная строка1. из которой невозможно восстановить исх. пароль.
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  # Служебный метод, преобразующий строку в 16-ричный формат для удобства хранения
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  # Основной метод для аутентификации юзера (логина). Проверяет email и пароль,
+  # если пользователь с такой комбинацией есть в базе, возвращает этого
+  # пользователя. Если нет — возвращает nil.
+  def self.authenticate(email, password)
+    # Сперва находим кандидата по email
+    user = find_by(email: email)
+
+    # Если пользователь не найден, возвращает nil
+    return nil unless user.present?
+
+    # Формируем хэш пароля из того, что передали в метод
+    hashed_password = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(
+        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+      )
+    )
+
+    # Обратите внимание: сравнивается password_hash, а оригинальный пароль так
+    # никогда и не сохраняется нигде. Если пароли совпали, возвращаем
+    # пользователя.
+    return user if user.password_hash == hashed_password
+
+    # Иначе, возвращаем nil
+    nil
+  end
 end
