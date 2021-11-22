581e1bbc1de6dcea3a898659a457f8abe79e2b97
Stanislav A. Timanov <1694569@mail.ru>
2021-11-22 15:49:28 +0300

    User Edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   # root to: 'users#index'
 
-  resources :users
+  resources :users, except: [:destroy]
   resources :questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
