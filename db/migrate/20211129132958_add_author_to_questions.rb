6a6ded9a8f5fcf5ac70ea9df75cdfe70bce91e51
Stanislav A. Timanov <1694569@mail.ru>
2021-12-06 12:18:23 +0300

    Change Migration Add Column Author

--- db/migrate/20211129132958_add_author_to_questions.rb
+++ db/migrate/20211129132958_add_author_to_questions.rb
@@ -1,7 +1,6 @@
 class AddAuthorToQuestions < ActiveRecord::Migration[6.1]
   def change
     add_column :questions, :author, :integer
-    add_index :questions, :author_id
 
     add_reference :questions, :author
   end
