5c73d3c17c1f8e84f51a0e1a4d7841ca743a6073
StanTim <1694569@mail.ru>
2021-12-21 15:38:46 +0300

    redirect _path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
     if @user.present?
       session[:user_id] = @user.id
-      redirect_to root_url, notice: 'вы успешно залогинились'
+      redirect_to root_path, notice: 'вы успешно залогинились'
     else
       flash.now.alert = 'Неправильный мэйл или пароль'
       render :new
@@ -28,6 +28,6 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
 
     # Редиректим пользователя на главную с сообщением
-    redirect_to root_url, notice: 'Вы разлогинились! Приходите еще!'
+    redirect_to root_path, notice: 'Вы разлогинились! Приходите еще!'
   end
 end
