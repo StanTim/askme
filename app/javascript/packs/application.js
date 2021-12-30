98fc147b7c5608ed255f7d3855ac0b93c54e8c0a
StanTim <1694569@mail.ru>
2021-12-30 20:16:28 +0300

    Turbolinks return

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
+Turbolinks.start()
 ActiveStorage.start()
 
 // Этот код добавляет обработчик события выдвигания формы при клике на кнопку
