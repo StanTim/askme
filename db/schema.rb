b7787e1b65a467888ee03728970a1dd2fe0f8c3d
StanTim <1694569@mail.ru>
2022-02-02 15:05:27 +0300

    add uniq index for email and username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_071550) do
+ActiveRecord::Schema.define(version: 2022_02_02_120355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_12_10_071550) do
     t.string "password_salt"
     t.string "avatar_url"
     t.string "color", default: "#005a55"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "questions", "users"
