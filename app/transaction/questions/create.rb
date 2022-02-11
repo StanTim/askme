f9611736852dbe2037fbe422b9bbdcbb146c050e
StanTim <1694569@mail.ru>
2022-02-11 15:03:44 +0300

    add create transaction

---
+++ app/transaction/questions/create.rb
@@ -0,0 +1,36 @@
+module Questions
+  class Create < BaseTransaction
+    step :build_model
+    step :check_recaptcha
+    step :validation
+    step :persistence
+
+    def build_model(input)
+      question = Question.new(author: input[:current_user], **input[:params])
+
+      Success(input.merge(question: question))
+    end
+
+    def check_recaptcha(input)
+      if input[:current_user].present? || RecaptchaCheck.not_bot?(input[:recaptcha_token], 'question')
+        Success(input)
+      else
+        Failure(input)
+      end
+    end
+
+    def validation(input)
+      if input[:question].valid?
+        Success(input)
+      else
+        Failure(input)
+      end
+    end
+
+    def persistence(input)
+      QuestionSave.(input[:question])
+
+      Success(input)
+    end
+  end
+end
