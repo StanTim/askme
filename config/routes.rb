6474f5976eb959034558b70daff724989b5b8a49
StanTim <1694569@mail.ru>
2022-02-12 09:23:40 +0300

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # Ресурс вопросов
   resources :questions, except: [:show, :new, :index]
 
-  # Ресурс хештеги
+  # Ресурс хештеги. Стандартный адрес по соглашению для запросов :id заменен
+  # на :text. - добавлено , params: :text, если не добавлять, то по-умолчанию будет :id
   resources :hashtags, only: :show, params: :text
 
   # Синонимы путей — в дополнение к созднным в ресурсах выше.
