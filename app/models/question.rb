ed9e8494c5de02956a7da9ef98a09000355ec261
StanTim <1694569@mail.ru>
2022-02-12 09:20:40 +0300

    remove mistake with :destroy

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
-  has_many :hashtag_questions, dependent: destroy
+  has_many :hashtag_questions, dependent: :destroy
   has_many :hashtags, through: :hashtag_questions
 
   validates :text,
