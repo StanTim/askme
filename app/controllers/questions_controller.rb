3bb4ef4d05fb47b92736e1a522a525ec8a137314
StanTim <1694569@mail.ru>
2022-02-13 18:44:51 +0300

    replace all  code from webinar

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,96 +1,60 @@
-# Контроллер, управляющий вопросами. Действия:
-#
-# 1. Редактирует вопрос
-# 2. Создает вопрос
-# 3. Удаляет вопрос
-
 class QuestionsController < ApplicationController
-  # Инициализируем объект вопрос для экшенов кроме :edit, :update, :destroy
-  before_action :load_question, only: [:edit, :update, :destroy]
-
-  # Проверяем имеет ли юзер доступ к экшену для всех дествий, кроме задавания
-  # вопроса, это действие может вызвать даже неавторизованный пользователь.
+  before_action :load_question, only: %i[destroy edit update]
   before_action :authorize_user, except: [:create]
 
-  # Действие edit будет отзываться по адресу /questions/:id/edit, например,
-  # /questions/1/edit
-  #
-  # Перед этим действием сработает before_action :load_questions и в переменной
-  # @question у нас будет лежать вопрос с нужным id равным params[:id].
+  def create
+    Questions::Create.(
+      params: question_create_params,
+        current_user: current_user,
+        recaptcha_token: params[:recaptcha_token]
+    ) do |m|
+      m.failure :check_recaptcha do |result|
+        redirect_to user_path(result[:question].user), notice: 'вы не прошли верификацию'
+      end
+
+      m.failure :validation do |result|
+        @question = result[:question]
+        render :edit
+      end
+
+      m.success do |result|
+        redirect_to user_path(result[:question].user), notice: 'вопрос задан'
+      end
+    end
+  end
+
   def edit
   end
 
-  # Действие create будет отзываться при POST-запросе по адресу /questions из
-  # формы нового вопроса, которая находится в шаблоне на странице
-  # /questions/edit
-  def create
-    @question = Question.new(question_params)
-
-    # Запись автора вопроса в поле author.
-    @question.author = current_user
-
-    if @question.save
-
-      redirect_to user_path(@question.user), notice: 'Вопрос задан'
-    else
-      render :edit
-    end
-  end
-
-  # Действие update будет отзываться при PUT-запросе из формы редактирования
-  # вопроса, которая находится по адресу /questions/:id, например, /questions/1
-  #
-  # Перед этим действием сработает before_action :load_questions и в переменной
-  # @question у нас будет лежать вопрос с нужным id равным params[:id].
   def update
-    if @question.update(question_params)
-      redirect_to user_path(@question.user), notice: 'Вопрос сохранен'
+    if @question.update(question_update_params)
+      redirect_to user_path(@question.user), notice: 'вопрос изменён'
     else
       render :edit
     end
   end
 
-  # Действие destroy будет отзываться при DELETE-запросе со страницы
-  # пользователя, которая находится по адресу /users/:id/show.
-  #
-  # Перед этим действием сработает before_action :load_questions и в переменной
-  # @question у нас будет лежать вопрос с нужным id равным params[:id].
   def destroy
-    # Перед тем, как удалять вопрос, сохраним пользователя, чтобы знать, куда
-    # редиректить после удаления.
     user = @question.user
-
-    # Удаляем вопрос
     @question.destroy
-
-    # Отправляем пользователя на страницу адресата вопроса с сообщением
-    redirect_to user_path(user), notice: 'Вопрос удален :('
+    redirect_to user_path(user), notice: 'вопрос удален'
   end
 
   private
 
-  # Если загруженный из базы вопрос не пренадлежит текущему залогиненному
-  # пользователю — посылаем его с помощью описанного в контроллере
-  # ApplicationController метода reject_user.
   def authorize_user
     reject_user unless @question.user == current_user
   end
 
-  # Загружаем из базы запрошенный вопрос, находя его по params[:id].
   def load_question
     @question = Question.find(params[:id])
   end
 
-  # Явно задаем список разрешенных параметров для модели Question. Мы говорим,
-  # что у хэша params должен быть ключ :question. Значением этого ключа может
-  # быть хэш с ключами: :user_id и :text. Другие ключи будут отброшены.
-  def question_params
-    # Защита от уязвимости: если текущий пользователь — адресат вопроса,
-    # он может менять ответы на вопрос,  ему доступно поле :answer.
-    if current_user.present?
-      params.require(:question).permit(:user_id, :text, :answer)
-    else
-      params.require(:question).permit(:user_id, :text)
-    end
+  def question_create_params
+    params.require(:question).permit(:user_id, :text)
+  end
+
+  def question_update_params
+    params.require(:question).permit(:answer)
   end
 end
